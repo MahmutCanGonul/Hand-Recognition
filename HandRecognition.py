e6dedcdf309065072e654931283746e7e22c2c1a
Mahmut Can Gönül <75094927+MahmutCanGonul@users.noreply.github.com>
2021-12-05 15:23:37 +0300

    Update HandRecognition.py

--- HandRecognition.py
+++ HandRecognition.py
@@ -15,11 +15,6 @@ camera = cv2.VideoCapture(0)
 
 if camera == None:
     print("I can not find camera") 
-    
-            
-     
-
-
 
 mp_hands = mp.solutions.hands
 hands = mp_hands.Hands(max_num_hands = 2, min_detection_confidence = 0.8)
@@ -27,6 +22,12 @@ mp_draw = mp.solutions.drawing_utils
  
  
 is_exit = False
+ 
+hold_posX = []
+hold_posY=[]
+count_time=0
+count_hands=0
+
 
  
 while True:
@@ -37,7 +38,7 @@ while True:
     hand_process = hands.process(imgRGB)
     
     height,width,channel = img.shape
-    
+    count_time+=1
     if hand_process.multi_hand_landmarks != None:
     
         for hand_landmarks in hand_process.multi_hand_landmarks:
@@ -47,9 +48,9 @@ while True:
                 """ # This code put the number on fingers
                 cv2.putText(img,str(fingers_num),(positionX,positionY),cv2.FONT_HERSHEY_SIMPLEX,1,(255,255,0),2)
                 """
+                    
                 if fingers_num == 4:
                     cv2.circle(img,(positionX,positionY),15,(255,255,255),cv2.FILLED)
-                """    
                 if fingers_num == 8:
                     cv2.circle(img,(positionX,positionY),15,(255,255,255),cv2.FILLED)
                 if fingers_num == 12:
@@ -58,19 +59,37 @@ while True:
                     cv2.circle(img,(positionX,positionY),15,(255,255,255),cv2.FILLED)
                 if fingers_num == 20:
                     cv2.circle(img,(positionX,positionY),15,(255,255,255),cv2.FILLED)
-               """
                 
+                """
                 if fingers_num > 4 and landmark.y < hand_landmarks.landmark[2].y:
                      break
-                
-                """
-                print(fingers_num,landmark.x,landmark.y)
-                """
                
                 if fingers_num == 20 and landmark.y > hand_landmarks.landmark[2].y:
                      is_exit=True
                      print("exit")
                       
+                
+                print(fingers_num,landmark.x,landmark.y)
+               """
+               
+                if len(hold_posX) > 0 and len(hold_posY) > 0:
+                   if fingers_num == 4 and hold_posX[len(hold_posX)-1] != positionX and hold_posY[len(hold_posY)-1] != positionY:
+                       count_hands+=1
+                       print("MOVE!")
+                   elif fingers_num == 4 and hold_posX[len(hold_posX)-1] == positionX and hold_posY[len(hold_posY)-1] == positionY:
+                       print("NOT MOVE!")
+                   
+                   if fingers_num == 4:
+                       hold_posX.append(positionX)
+                       hold_posY.append(positionY)
+                        
+                else:
+                    if fingers_num == 4:
+                        hold_posX.append(positionX)
+                        hold_posY.append(positionY)
+                        
+               
+               
                
                 
                 
@@ -78,12 +97,20 @@ while True:
                 
                  
             mp_draw.draw_landmarks(img,hand_landmarks,mp_hands.HAND_CONNECTIONS)
+    
+    else:
+         count_time+=1
+         
     if is_exit:
         break
-    
+     
     
     cv2.imshow("Camera",img)
     if cv2.waitKey(1) &  0xFF == ord("q"): # When you click two time  q camera is shutdown
+        result = float(count_hands/count_time)
+        print("Hand Skill Point:  %",str(result))
+        print("TIME: ",count_time)
+        print("HAND_RECOG: ",count_hands)
         break
 
 
